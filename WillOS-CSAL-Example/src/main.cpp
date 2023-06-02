16fb18b54b8c9bdba0e89b181d93c30b9b7eabf7
William Personal <william-personal@Williams-MBP.attlocal.net>
2023-06-02 05:25:09 -0700

    Example code for the Will-OS CSAL is here

--- WillOS-CSAL-Example/src/main.cpp
+++ WillOS-CSAL-Example/src/main.cpp
@@ -1,10 +1,12 @@
 #include "OS/OSThreadKernel.h"
+#include "CSAL/CSAL_SHARED/threads_init.h"
 
 void setup() {
   os_init();
+  threads_list_init();
 }
 
 void loop() {
-  //os_suspend_thread(os_current_id());
-  //_os_yield();
+  os_suspend_thread(os_current_id());
+  _os_yield();
 }
